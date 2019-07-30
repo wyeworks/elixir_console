e2fd8d2b1969ee96e5a0aa50e96d76fe859b9f3e
Ignacio Aguirrezabal <ignacio@wyeworks.com>
2019-07-30 17:49:51 -0300

    Show errors
    Co-authored-by: Leticia Errandonea <leticia@wyeworks.com>
    Co-authored-by: Jorge Bejar <jorge@wyeworks.com>

--- lib/live_view_demo_web/live/console_live.ex
+++ lib/live_view_demo_web/live/console_live.ex
@@ -7,21 +7,39 @@ defmodule LiveViewDemoWeb.ConsoleLive do
       <form phx-submit="execute">
         <input type="text" name="command" value="<%= @command %>"/>
         <%= if @result do %><pre><%= @result %></pre><% end %>
+        <%= if @error do %><pre style="background-color: #edcacd"><%= @error %></pre><% end %>
       </form>
     </div>
     """
   end
 
   def mount(_session, socket) do
-    {:ok, assign(socket, command: "", result: nil)}
+    {:ok, assign(socket, command: "", error: nil, result: nil)}
   end
 
   def handle_event("execute", %{"command" => command}, socket) do
-    {:noreply, assign(socket, command: command, result: execute_command(command))}
+    socket = reset_assigns(socket, command)
+
+    case execute_command(command) do
+      {:ok, result} -> {:noreply, assign(socket, result: result)}
+      {:error, error} -> {:noreply, assign(socket, error: error)}
+    end
   end
 
   defp execute_command(command) do
     {result, _} = Code.eval_string(command)
-    inspect(result)
+    {:ok, inspect(result)}
+  catch
+    kind, error ->
+      error = Exception.normalize(kind, error)
+      {:error, inspect(error)}
+  end
+
+  defp reset_assigns(socket, command) do
+    assign(socket,
+      command: command,
+      error: nil,
+      result: nil
+    )
   end
 end
