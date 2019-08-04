a999ff81890a535040c95f9475dcfa4d829621c6
Ignacio Aguirrezabal <ignacio@wyeworks.com>
2019-08-04 16:45:34 -0300

    Store bindings in socket

--- lib/live_view_demo_web/live/console_live.ex
+++ lib/live_view_demo_web/live/console_live.ex
@@ -14,21 +14,21 @@ defmodule LiveViewDemoWeb.ConsoleLive do
   end
 
   def mount(_session, socket) do
-    {:ok, assign(socket, command: "", error: nil, result: nil)}
+    {:ok, assign(socket, command: "", error: nil, result: nil, bindings: [])}
   end
 
   def handle_event("execute", %{"command" => command}, socket) do
     socket = reset_assigns(socket, command)
 
-    case execute_command(command) do
-      {:ok, result} -> {:noreply, assign(socket, result: result)}
+    case execute_command(command, socket.assigns.bindings) do
+      {:ok, result, bindings} -> {:noreply, assign(socket, result: result, bindings: bindings)}
       {:error, error} -> {:noreply, assign(socket, error: error)}
     end
   end
 
-  defp execute_command(command) do
-    {result, _} = Code.eval_string(command)
-    {:ok, inspect(result)}
+  defp execute_command(command, bindings) do
+    {result, bindings} = Code.eval_string(command, bindings)
+    {:ok, inspect(result), bindings}
   catch
     kind, error ->
       error = Exception.normalize(kind, error)
