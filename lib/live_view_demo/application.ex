0ba7d3b0b307cd4f1eda00f4e130e596f10e2e27
Jorge Bejar <jorge@wyeworks.com>
2019-08-08 11:22:51 -0300

    Remove Ecto

--- lib/live_view_demo/application.ex
+++ lib/live_view_demo/application.ex
@@ -8,8 +8,6 @@ defmodule LiveViewDemo.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
-      # Start the Ecto repository
-      LiveViewDemo.Repo,
       # Start the endpoint when the application starts
       LiveViewDemoWeb.Endpoint
       # Starts a worker by calling: LiveViewDemo.Worker.start_link(arg)
