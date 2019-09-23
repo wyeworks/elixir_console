7343caba08c69ecd7cd6263a7caba2736f4cd443
Jorge Bejar <jorge@wyeworks.com>
2019-09-23 15:46:46 -0300

    Consider most of Elixir modules

--- lib/live_view_demo/contextual_help.ex
+++ lib/live_view_demo/contextual_help.ex
@@ -1,28 +1,31 @@
 defmodule ContextualHelp do
 
+  @modules [Access, Enum, Keyword, List, Map, MapSet, Range, Stream, File, IO, Path, Port, StringIO, System, Calendar,
+            Agent, Application, Config, DynamicSupervisor, GenEvent, Node, Process, Registry, Task, Collectable, Enumerable,
+            Inspect, Protocol, Code, Macro]
+
   def compute(command) do
-    # TODO Enum is harcoded
-    docs = docs(Enum)
+    docs = retrive_docs(@modules)
 
     {:ok, expr} = Code.string_to_quoted(command)
     functions = find_functions(expr, [])
     add_documentation(command, functions, docs)
   end
 
-  def find_functions({{:., _, [{_, _, [:Enum]}, func_name]}, _, params}, acc) do
-    acc = acc ++ [%{func_name: func_name, func_ary: Enum.count(params)}]
+  def find_functions({{:., _, [{_, _, [module]}, func_name]}, _, params}, acc) do
+    acc = acc ++ [%{module: module, func_name: func_name, func_ary: Enum.count(params)}]
     Enum.reduce(params, acc, fn node, acc -> find_functions(node, acc) end)
   end
 
   def find_functions(_, acc), do: acc
 
-  def add_documentation(command, [%{func_name: func_name, func_ary: func_ary} | rest], docs) do
-    regex = ~r/Enum.#{Regex.escape(Atom.to_string(func_name))}/
+  def add_documentation(command, [%{module: module, func_name: func_name, func_ary: func_ary} | rest], docs) do
+    regex = ~r/#{module}.#{Regex.escape(Atom.to_string(func_name))}/
     [before, _, remaining_command] = Regex.split(regex, command, include_captures: true, parts: 2)
 
-    case docs[%{func_name: "Enum.#{func_name}", func_ary: func_ary}] do
-      nil -> [before, "Enum.#{func_name}"] ++ add_documentation(remaining_command, rest, docs)
-      doc -> [before, {"Enum.#{func_name}", doc}] ++ add_documentation(remaining_command, rest, docs)
+    case docs[%{func_name: "#{module}.#{func_name}", func_ary: func_ary}] do
+      nil -> [before, "#{module}.#{func_name}"] ++ add_documentation(remaining_command, rest, docs)
+      doc -> [before, {"#{module}.#{func_name}", doc}] ++ add_documentation(remaining_command, rest, docs)
     end
   end
 
@@ -30,17 +33,22 @@ defmodule ContextualHelp do
     [command]
   end
 
-  def docs(module) do
+  def retrive_docs([module | remaining_modules]) do
     {:docs_v1, _, :elixir, _, _, _, list} = Code.fetch_docs(module)
 
-    Enum.reduce(list, %{}, fn function, acc ->
+    docs = Enum.reduce(list, %{}, fn function, acc ->
       case function do
         {{:function, func_name, func_ary}, _, _header, %{"en" => docs}, _} ->
           {:ok, html_doc, _} = Earmark.as_html(docs)
-          Map.put(acc, %{func_name: "Enum.#{func_name}", func_ary: func_ary}, %{header: "Enum.#{func_name}/#{func_ary}", docs: html_doc})
+          [module_name] = Module.split(module)
+          Map.put(acc, %{func_name: "#{module_name}.#{func_name}", func_ary: func_ary}, %{header: "#{module_name}.#{func_name}/#{func_ary}", docs: html_doc})
         _ ->
           acc
       end
     end)
+
+    Map.merge(docs, retrive_docs(remaining_modules))
   end
+
+  def retrive_docs([]), do: %{}
 end
