6000ab40f755ff315e9ee63fc17800bc5e97df80
Jorge Bejar <jorge@wyeworks.com>
2019-09-24 11:13:24 -0300

    Use GenServer to have Elixir docs in memory

--- lib/live_view_demo/application.ex
+++ lib/live_view_demo/application.ex
@@ -8,10 +8,8 @@ defmodule LiveViewDemo.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
-      # Start the endpoint when the application starts
-      LiveViewDemoWeb.Endpoint
-      # Starts a worker by calling: LiveViewDemo.Worker.start_link(arg)
-      # {LiveViewDemo.Worker, arg},
+      LiveViewDemoWeb.Endpoint,
+      LiveViewDemo.Documentation
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
--- lib/live_view_demo/contextual_help.ex
+++ lib/live_view_demo/contextual_help.ex
@@ -1,43 +1,9 @@
 defmodule LiveViewDemo.ContextualHelp do
-  @modules [
-    Access,
-    Enum,
-    Keyword,
-    List,
-    Map,
-    MapSet,
-    Range,
-    Stream,
-    File,
-    IO,
-    Path,
-    Port,
-    StringIO,
-    System,
-    Calendar,
-    Agent,
-    Application,
-    Config,
-    DynamicSupervisor,
-    GenEvent,
-    Node,
-    Process,
-    Registry,
-    Task,
-    Collectable,
-    Enumerable,
-    Inspect,
-    Protocol,
-    Code,
-    Macro
-  ]
 
   def compute(command) do
-    docs = retrive_docs(@modules)
-
     {:ok, expr} = Code.string_to_quoted(command)
     functions = find_functions(expr, [])
-    add_documentation(command, functions, docs)
+    add_documentation(command, functions)
   end
 
   defp find_functions({{:., _, [{_, _, [module]}, func_name]}, _, params}, acc) do
@@ -53,48 +19,24 @@ defmodule LiveViewDemo.ContextualHelp do
 
   defp add_documentation(
         command,
-        [%{module: module, func_name: func_name, func_ary: func_ary} | rest],
-        docs
+        [%{module: module, func_name: func_name, func_ary: func_ary} | rest]
       ) do
     regex = ~r/#{module}.#{Regex.escape(Atom.to_string(func_name))}/
     [before, _, remaining_command] = Regex.split(regex, command, include_captures: true, parts: 2)
 
-    case docs[%{func_name: "#{module}.#{func_name}", func_ary: func_ary}] do
+    docs = LiveViewDemo.Documentation.get_doc(%{func_name: "#{module}.#{func_name}", func_ary: func_ary})
+
+    case docs do
       nil ->
-        [before, "#{module}.#{func_name}"] ++ add_documentation(remaining_command, rest, docs)
+        [before, "#{module}.#{func_name}"] ++ add_documentation(remaining_command, rest)
 
       doc ->
         [before, {"#{module}.#{func_name}", doc}] ++
-          add_documentation(remaining_command, rest, docs)
+          add_documentation(remaining_command, rest)
     end
   end
 
-  defp add_documentation(command, [], _) do
+  defp add_documentation(command, []) do
     [command]
   end
-
-  defp retrive_docs([module | remaining_modules]) do
-    {:docs_v1, _, :elixir, _, _, _, list} = Code.fetch_docs(module)
-
-    docs =
-      Enum.reduce(list, %{}, fn function, acc ->
-        case function do
-          {{:function, func_name, func_ary}, _, _header, %{"en" => docs}, _} ->
-            {:ok, html_doc, _} = Earmark.as_html(docs)
-            [module_name] = Module.split(module)
-
-            Map.put(acc, %{func_name: "#{module_name}.#{func_name}", func_ary: func_ary}, %{
-              header: "#{module_name}.#{func_name}/#{func_ary}",
-              docs: html_doc
-            })
-
-          _ ->
-            acc
-        end
-      end)
-
-    Map.merge(docs, retrive_docs(remaining_modules))
-  end
-
-  defp retrive_docs([]), do: %{}
 end
