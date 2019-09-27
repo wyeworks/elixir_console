582f3cd1b65900650311b92c4ae1c64d0bb72909
Jorge Bejar <jorge@wyeworks.com>
2019-09-27 10:42:29 -0300

    Fix web app host

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :live_view_demo, LiveViewDemoWeb.Endpoint,
-  url: [scheme: "https", host: "elixir-console-wye", port: 443],
+  url: [scheme: "https", host: "elixir-console-wye.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
