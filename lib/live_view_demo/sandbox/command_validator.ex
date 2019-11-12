54283353672efcd6f4761e9ce1c0b581e947d974
Jorge Bejar <jorge@wyeworks.com>
2019-11-12 17:04:19 -0300

    Add missing moduledoc

--- lib/live_view_demo/sandbox/command_validator.ex
+++ lib/live_view_demo/sandbox/command_validator.ex
@@ -1,4 +1,11 @@
 defmodule LiveViewDemo.Sandbox.CommandValidator do
+  @moduledoc """
+  Check if a given Elixir code from untrusted sources is safe to be executed in
+  the sandbox. This module also defines a behavior to be implemented by
+  validator modules, providing a mechanism to compose individual safety checks
+  over the command.
+  """
+
   @type ast :: Macro.t()
   @callback validate(ast()) :: :ok | {:error, String.t()}
 
