761fa47963a749ff457efd6099950ce23df289e6
Jorge Bejar <jorge@wyeworks.com>
2019-11-18 15:44:41 -0300

    Coding style fixes in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+import css from '../css/app.css';
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -9,45 +9,45 @@ import css from "../css/app.css"
 //
 // Import dependencies
 //
-import "phoenix_html"
+import 'phoenix_html';
 
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
 
-import {Socket} from "phoenix"
-import LiveSocket from "phoenix_live_view"
+import {Socket} from 'phoenix';
+import LiveSocket from 'phoenix_live_view';
 
-let Hooks = {}
+let Hooks = {};
 Hooks.CommandInput = {
   mounted() {
     const pushEvent = this.pushEvent;
-    const input = document.getElementById("commandInput");
+    const input = document.getElementById('commandInput');
     const sendCursorPosition = e => {
       if (input.selectionStart === input.selectionEnd) {
-        this.pushEvent("caret-position", { position: input.selectionEnd });
+        this.pushEvent('caret-position', { position: input.selectionEnd });
       }
     };
 
-    ["keyup", "click", "focus"].forEach(event => {
+    ['keyup', 'click', 'focus'].forEach(event => {
       input.addEventListener(event, sendCursorPosition, true);
-    })
+    });
   },
   updated() {
-    let newValue = this.el.getAttribute("data-input_value")
-    if (newValue !== "") {
+    let newValue = this.el.getAttribute('data-input_value');
+    if (newValue !== '') {
       this.el.value = newValue;
     }
   }
 }
 
-let liveSocket = new LiveSocket("/live", Socket, {hooks: Hooks})
-liveSocket.connect()
+let liveSocket = new LiveSocket('/live', Socket, {hooks: Hooks});
+liveSocket.connect();
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  let input = document.getElementById("commandInput");
-  input.addEventListener("keydown", function(e) {
+document.addEventListener('DOMContentLoaded', function(event) {
+  let input = document.getElementById('commandInput');
+  input.addEventListener('keydown', function(e) {
     if (e.keyCode === 9) {
       e.preventDefault();
     }
