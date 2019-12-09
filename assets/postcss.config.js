cc30a0dcac220d19aba806ef7a6655d1713b0667
Jorge Bejar <jorge@wyeworks.com>
2019-12-09 18:44:27 -0300

    Fix code style issues in assets

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -3,4 +3,4 @@ module.exports = {
     require('tailwindcss'),
     require('autoprefixer')
   ]
-}
+};
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -42,7 +42,7 @@ module.exports = (env, options) => ({
             }
           }
         ]
-      },
+      }
     ]
   },
   plugins: [
