708f31e604ca3d2cb1250ec3824d0995ce4b6e99
Jorge Bejar <jorge@wyeworks.com>
2019-12-12 10:54:27 -0300

    App domain is configurable via env var

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :elixir_console, ElixirConsoleWeb.Endpoint,
-  url: [scheme: "https", host: "elixir-console-wye.herokuapp.com", port: 443],
+  url: [scheme: "https", host: System.get_env("APP_HOST"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
