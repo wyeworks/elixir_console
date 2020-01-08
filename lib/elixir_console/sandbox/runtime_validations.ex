0c43ca0f016761f734ca5bfc3953648ab06951c1
Jorge Bejar <jorge@wyeworks.com>
2020-01-08 17:52:06 -0300

    Complete changes in safe_invoation fixing issues with macros

--- lib/elixir_console/sandbox/runtime_validations.ex
+++ lib/elixir_console/sandbox/runtime_validations.ex
@@ -15,9 +15,26 @@ defmodule ElixirConsole.Sandbox.RuntimeValidations do
   """
   def get_augmented_ast(command) do
     ast = Code.string_to_quoted!(command)
-    {augmented_ast, _result} = Macro.postwalk(ast, nil, &add_safe_invocation(&1, &2))
+    {augmented_ast, _result} = Macro.prewalk(ast, nil, &add_safe_invocation(&1, &2))
 
     augmented_ast
+  end
+
+  defp add_safe_invocation(
+         {:|>, outer_meta,
+          [first_param, {{:., meta, [callee, function]}, meta, remaining_params}]},
+         acc
+       ) do
+    params = [first_param | remaining_params]
+
+    elem =
+      {{:., outer_meta,
+        [
+          {:__aliases__, meta, @this_module},
+          :safe_invocation
+        ]}, meta, [callee, function, params]}
+
+    {elem, acc}
   end
 
   defp add_safe_invocation({{:., meta, [callee, function]}, outer_meta, params}, acc) do
@@ -54,18 +71,18 @@ defmodule ElixirConsole.Sandbox.RuntimeValidations do
 
   # TODO generalize to other macros from Kernel and Integer (and for other modules?)
   def safe_invocation(Kernel, :to_string, params) do
-    apply(&(Kernel.to_string(&1)), params)
+    apply(&to_string(&1), params)
   end
 
   def safe_invocation(Integer, :is_odd, params) do
     require Integer
-    apply(&(Integer.is_odd(&1)), params)
-  end
-  def safe_invocation(Integer, :is_even, params) do
-    require Integer
-    apply(&(Integer.is_even(&1)), params)
+    apply(&Integer.is_odd(&1), params)
   end
 
+  def safe_invocation(Integer, :is_even, params) do
+    require Integer
+    apply(&Integer.is_even(&1), params)
+  end
 
   def safe_invocation(callee, function, params) do
     if ElixirConsole.Sandbox.Util.is_erlang_module?(callee) do
--- test/elixir_console/sandbox/runtime_validations_test.exs
+++ test/elixir_console/sandbox/runtime_validations_test.exs
@@ -8,7 +8,7 @@ defmodule ElixirConsole.Sandbox.RuntimeValidationsTest do
     assert original_ast == RuntimeValidations.get_augmented_ast(command)
   end
 
-  test "inserts an inline call when dot operator is used" do
+  test "replaces invocation when dot operator is used" do
     command = "Enum.count([2])"
 
     {:ok, expected_ast} =
@@ -19,39 +19,49 @@ defmodule ElixirConsole.Sandbox.RuntimeValidationsTest do
     assert expected_ast == RuntimeValidations.get_augmented_ast(command)
   end
 
-  @tag :pending
-  test "inserts an inline call when pipe operator is used" do
-    command = "[] |> Enum.count"
-
-    {:ok, expected_ast} =
-      Code.string_to_quoted(
-        "ElixirConsole.Sandbox.RuntimeValidations.safe_invocation(Enum, :count, [[]])"
-      )
-
-    assert expected_ast == RuntimeValidations.get_augmented_ast(command)
-  end
-
-  @tag :pending
-  test "inserts inline calls within nested expressions" do
+  test "replace invocations within nested expressions" do
     command = "Enum.count(Enum.map([1,2,3], &(&1 * 2)))"
 
     {:ok, expected_ast} =
       Code.string_to_quoted(
-        "ElixirConsole.Sandbox.RuntimeValidations.safe_invocation(Enum, :count).count(" <>
-          "ElixirConsole.Sandbox.RuntimeValidations.safe_invocation(Enum, :map).map([1,2,3], &(&1 * 2)))"
+        "ElixirConsole.Sandbox.RuntimeValidations.safe_invocation(Enum, :count, [" <>
+          "ElixirConsole.Sandbox.RuntimeValidations.safe_invocation(Enum, :map, [[1,2,3], &(&1 * 2)])" <>
+          "])"
       )
 
     assert expected_ast == RuntimeValidations.get_augmented_ast(command)
   end
 
-  @tag :pending
-  test "inserts an inline call when string interpolation is used" do
-    command = "\"Hello \#{:world}\""
+  test "changes to direct invocation and replaces invocation when pipe operator is used" do
+    command = "[1,2,3] |> Enum.map(fn n -> n * 2 end) |> Enum.count"
 
     {:ok, expected_ast} =
       Code.string_to_quoted(
-        "<<\"Hello \", ElixirConsole.Sandbox.RuntimeValidations.safe_invocation(Kernel, :to_string).to_string(:world)::binary>>"
+        "ElixirConsole.Sandbox.RuntimeValidations.safe_invocation(Enum, :count, [" <>
+          "ElixirConsole.Sandbox.RuntimeValidations.safe_invocation(Enum, :map, [[1,2,3], fn n -> n * 2 end])" <>
+          "])"
       )
+
+    assert expected_ast == RuntimeValidations.get_augmented_ast(command)
+  end
+
+  test "makes a safe invocation when string interpolation is used" do
+    command = "\"Hello \#{:world}\""
+
+    expected_ast =
+      {:<<>>, [line: 1],
+       [
+         "Hello ",
+         {:"::", [line: 1],
+          [
+            {{:., [line: 1],
+              [
+                {:__aliases__, [line: 1], [:ElixirConsole, :Sandbox, :RuntimeValidations]},
+                :safe_invocation
+              ]}, [line: 1], [Kernel, :to_string, [:world]]},
+            {:binary, [line: 1], nil}
+          ]}
+       ]}
 
     assert expected_ast == RuntimeValidations.get_augmented_ast(command)
   end
