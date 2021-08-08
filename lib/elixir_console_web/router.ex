e468b9a57d6b89566c403ca2edf4f3e6363e8b2a
Daniel Gomez de Souza <daniel@wyeworks.com>
2021-08-08 19:51:44 -0300

    Upgrade LiveView to 0.9.0
    This update requires to add the plug put_root_layout to the browser
    pipeline in order to render the layout since live views won't default to
    the app layout anymore. Since this doesn't have any regular views, just
    one liveview, we don't need to keep the app layout.

--- lib/elixir_console_web/router.ex
+++ lib/elixir_console_web/router.ex
@@ -5,6 +5,7 @@ defmodule ElixirConsoleWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_live_flash
+    plug :put_root_layout, {ElixirConsoleWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
