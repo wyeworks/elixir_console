6ac4c04b0af50ba6cb1afe65f2b4d00f49a0adf5
Daniel Gomez de Souza <daniel@wyeworks.com>
2021-08-10 10:01:24 -0300

    Remove double quotes as per sourcelevel-bot suggestion

--- assets/js/app.js
+++ assets/js/app.js
@@ -44,7 +44,7 @@ Hooks.CommandInput = {
   }
 };
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+let csrfToken = document.querySelector('meta[name=\'csrf-token\']').getAttribute('content');
 let liveSocket = new LiveSocket('/live', Socket, {hooks: Hooks, params: {_csrf_token: csrfToken}});
 liveSocket.connect();
 
