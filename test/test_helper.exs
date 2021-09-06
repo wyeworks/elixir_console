cfa60e24194987cae2056e0f03079ad75f1ce73c
Gonzalo <grzuy0@gmail.com>
2021-09-06 14:22:52 -0300

    Attempt to reproduce command input not cleaning up bug

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,4 @@
 ExUnit.start(exclude: [:pending])
+{:ok, _} = Application.ensure_all_started(:wallaby)
+Application.put_env(:wallaby, :base_url, ElixirConsoleWeb.Endpoint.url())
+Application.put_env(:wallaby, :screenshot_on_failure, true)
