490eb872293619c27568c2d485664f37c85254c5
Gonzalo <grzuy0@gmail.com>
2021-09-29 11:25:00 -0300

    Remove unnecessary explicit listing of phoenix compiler since phoenix 1.6

--- mix.exs
+++ mix.exs
@@ -7,7 +7,6 @@ defmodule ElixirConsole.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12.3",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
