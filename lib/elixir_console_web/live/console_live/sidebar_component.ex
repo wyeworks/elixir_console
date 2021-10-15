3f5873616f0e7f3691cc097e7f6c259f10b00a7b
Daniel Gomez de Souza <daniel@wyeworks.com>
2021-10-15 19:01:05 -0300

    Declare props to remove warnings

--- lib/elixir_console_web/live/console_live/sidebar_component.ex
+++ lib/elixir_console_web/live/console_live/sidebar_component.ex
@@ -9,6 +9,12 @@ defmodule ElixirConsoleWeb.ConsoleLive.SidebarComponent do
 
   alias ElixirConsoleWeb.ConsoleLive.{Bindings, Suggestions, ContextualHelp, Instructions}
 
+  prop sandbox, :map
+
+  prop suggestions, :list
+
+  prop contextual_help, :map
+
   def render(assigns) do
     ~F"""
     <div class="w-full sm:w-32 md:w-1/3 h-32 sm:h-full bg-teal-800 text-gray-200 overflow-scroll flex flex-col">
