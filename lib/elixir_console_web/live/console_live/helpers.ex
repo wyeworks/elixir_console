406c2aeee1f3a60e1c2164d67988c770778fb914
Daniel Gomez de Souza <daniel@wyeworks.com>
2022-01-28 09:46:38 -0300

    Convert prompt to heex template

--- lib/elixir_console_web/live/console_live/helpers.ex
+++ lib/elixir_console_web/live/console_live/helpers.ex
@@ -3,5 +3,9 @@ defmodule ElixirConsoleWeb.ConsoleLive.Helpers do
   This module provides very simple helpers to use in templates and avoid repetition
   """
 
-  def prompt, do: "> "
+  import Phoenix.LiveView.Helpers, only: [sigil_H: 2]
+
+  def prompt(assigns) do
+    ~H(> )
+  end
 end
